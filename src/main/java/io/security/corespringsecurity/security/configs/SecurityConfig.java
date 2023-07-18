1ae76874f415e66a9242bc47e10db865ac346a9f
ksb96 <ksebin96@gmail.com>
2023-07-18 23:14:04 +0900

    commit

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,60 +1,52 @@
 package io.security.corespringsecurity.security.configs;
 
+import static org.springframework.security.config.Customizer.*;
+
+import io.security.corespringsecurity.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
   private final UserDetailsService userDetailsService;
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService);
+  @Bean
+  public CustomUserDetailsService customUserDetailsService() {
+    return new CustomUserDetailsService();
   }
 
-  // 메모리 방식
-/*  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    String password = passwordEncoder().encode("1111");
-
-    auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-    auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-    auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN","USER", "MANAGER");
-  }*/
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .authorizeRequests()
-        .antMatchers("/", "/users").permitAll()
-        .antMatchers("/mypage").hasRole("USER")
-        .antMatchers("/messages").hasRole("MANAGER")
-        .antMatchers("/config").hasRole("ADMIN")
-        .anyRequest()
-        .authenticated()
+        .securityMatcher("/**")
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/", "/users").permitAll();
+          auth.requestMatchers("/mypage").hasRole("USER");
+          auth.requestMatchers("/messages").hasRole("MANAGER");
+          auth.requestMatchers("/config").hasRole("ADMIN");
+          auth.anyRequest().authenticated();
+        })
+        .formLogin(withDefaults());
 
-        .and()
-        .formLogin();
+    return http.build();
   }
 
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
   }
 
   @Bean
